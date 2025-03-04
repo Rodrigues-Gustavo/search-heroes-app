707be669e2979745f892f909c6af40dbeeb17730
Gustavo <gustavorr001@gmail.com>
2025-03-04 06:23:45 -0300

    busca de herói por id

--- src/services/marvelApi.tsx
+++ src/services/marvelApi.tsx
@@ -31,3 +31,26 @@ export async function buscarHeroisHome(nomeIniciaCom = '',nome = '', limite = 20
         return [];
     }
 }
+
+export async function buscarHeroi(id:string) {
+    
+    const ts = new Date().getTime().toString();
+
+    const hash = createHash("md5")
+    .update(ts + PRIVATE_KEY + PUBLIC_KEY)
+    .digest("hex");
+
+    try {
+        const response = await axios.get(`${BASE_URL}characters/${id}`, {
+            params: {
+            apikey: PUBLIC_KEY,
+            ts,
+            hash
+        },
+        });
+        return response.data.data.results;
+    } catch (erro) {
+        console.error("Erro ao buscar heróis:", erro);
+        return [];
+    }
+}
