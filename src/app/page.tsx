64250df705d816b12885cd67d5b5cc1961023f78
Gustavo <gustavorr001@gmail.com>
2025-03-03 21:15:25 -0300

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <h1>marvel - heroes</h1>
+    <header className={styles.header}>
+      <Image
+        src="/assets/logo/Group@3x.png"
+        alt="Logo do Grupo"
+        layout="intrinsic"
+        width={340}
+        height={0}
+      />
+      <h1>EXPLORE O UNIVERSO</h1>
+      <h5>Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</h5>
+    </header>
   );
 }
