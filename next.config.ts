825e9a87cc7fb9976f7cab4770b336f28e5c4f38
Gustavo <gustavorr001@gmail.com>
2025-03-03 21:10:43 -0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+require('dotenv').config();
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MARVEL_PUBLIC_KEY: process.env.MARVEL_PUBLIC_KEY,
+    MARVEL_PRIVATE_KEY: process.env.MARVEL_PRIVATE_KEY,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'i.annihil.us',
+        pathname: '**',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
